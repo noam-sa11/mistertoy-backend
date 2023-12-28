c579ee13a8540e80b8b84dbf63d83b5851f49935
Noam Saar <noamsaar11@gmail.com>
2023-12-28 21:00:51 +0200

    Add config

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+export default {
+    // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+    dbURL: 'mongodb://127.0.0.1:27017',
+    dbName: 'toy_db',
+}
